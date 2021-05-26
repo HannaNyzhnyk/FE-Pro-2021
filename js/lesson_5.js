837f2000f49f0fb19e553e170ecc998472855b94
nyzhnyk.ann@outlook.com <nyzhnyk.ann@outlook.com>
2021-05-26 14:19:53 +0300

    l72

--- js/lesson_5.js
+++ js/lesson_5.js
@@ -1,11 +1,11 @@
-let submitBtn = document.getElementById('SubmitBtn');
-submitBtn.onclick  = function() {
-    var user = {
-        name: document.getElementById("user-name").value,
-        email: document.getElementById("user-email").value,
-        age:document.getElementById("user-age").value,
-        comment:document.getElementById("comment").value,
-        country:document.getElementById("country").value
-    }
-    console.log(user);
-}
+// let submitBtn = document.getElementById('SubmitBtn');
+// submitBtn.onclick  = function() {
+//     var user = {
+//         name: document.getElementById("user-name").value,
+//         email: document.getElementById("user-email").value,
+//         age:document.getElementById("user-age").value,
+//         comment:document.getElementById("comment").value,
+//         country:document.getElementById("country").value
+//     }
+//     console.log(user);
+// }
