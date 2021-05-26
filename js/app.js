cea0839654c744477e21cce36cc2c016c1386eca
nyzhnyk.ann@outlook.com <nyzhnyk.ann@outlook.com>
2021-05-26 14:34:03 +0300

    l73

--- js/app.js
+++ js/app.js
@@ -1,37 +1,99 @@
-const renderCountries = countries => {
-    console.log(countries);
-    let htmlStr = '';
-    for(let country of countries) {
-        htmlStr += `<tr>
-            <td>${country.region}</td>
-            <td>${country.currencies.join(", ")}</td>
-            <td>${country.languages}</td>
-            <td><img src="${country.flag}" style="height: 50px;"></td>
-        </tr>`;
+window.onload = function() {
+    window.countries = null;
+
+    const setListeners = () => {
+        $('#search').on('keyup', e => {
+            let value = e.target.value;
+            value = value.toLowerCase();
+            let filteredCountries = window.countries.filter(country => country.name.toLowerCase().indexOf(value) > -1
+                || country.capital.toLowerCase().indexOf(value) > -1
+                || country.region.toLowerCase().indexOf(value) > -1);
+
+            renderCountries(filteredCountries);
+        });
+
+        $('table thead th.bg-warning').on('click touch', e => {
+            let field = e.currentTarget.innerText;
+            field = field.toLowerCase();
+            let numerableFields = ['population', 'area'];
+            let sortedCountries = window.countries.sort((a, b) => {
+                return numerableFields.indexOf(field) > -1 ? a[field] < b[field] : a[field] > b[field];
+            });
+            $('table thead th.bg-warning').removeClass('bg-danger text-white');
+            $(e.currentTarget).addClass('bg-danger text-white')
+            renderCountries(sortedCountries);
+        });
+
+        $('td').on('click', e => {
+            $('table tbody tr').removeClass('bg-dark text-white');
+            $(e.currentTarget).parent('tr').addClass('bg-dark text-white');
+        })
     }
 
-    console.log(htmlStr);
-    document.querySelector('table > tbody').innerHTML = htmlStr;
-}
-
-const getData = () => {
-    fetch('https://restcountries.eu/rest/v2/all')
-        .then(res => res.json()).then(data => {
-            let countries = data.map(country => {
-                return {
-                    region: country.region,
-                    flag: country.flag,
-                    languages: country.languages.map(language => {
-                        return language.name;
-                    }),
-                    currencies: country.currencies.map(currency => {
-                        return currency.name;
-                    })
-                };
-            });
+    const SelectTable  = () => {
+        $('.countries-select').change (e => {
+            $('#search').val("");
             renderCountries(countries);
-    });
-}
+            let countryName = e.currentTarget.value.replace(" ", "_");
+            if(countryName) {
+                let selector = `[data-name]:not([data-name=${(countryName)}])`;
+                let countryRow = $(selector);
+                countryRow.hide();
+                $(`[data-name=${(countryName)}]`).show();
+            } else {
+                $('[data-name]').show();
+            }
+        })
+    }
 
-document.addEventListener("DOMContentLoaded", () => { getData(); });
+    const renderOptions = countries => {
+        let selectStr = '<option value="">Not selected</option>';
+        for(let country of countries) {
+           selectStr += `<option value="${country.name}">${country.name}</option>`;
+        }
+        $('.countries-select').html(selectStr);
+    } 
 
+    const renderCountries = countries => {
+        let htmlStr = '';
+        for(let country of countries) {
+            htmlStr += `<tr data-name=${country.name.replace(" ", "_")}>
+            <td>${country.name}</td>
+            <td>${country.capital}</td>
+            <td>${country.region}</td>
+            <td>${country.population}</td>
+            <td>${country.area}</td>
+            <td>${country.currencies}</td>
+            <td>${country.languages}</td>
+            <td><img src="${country.flag}"  height="30px"/></td>
+        </tr>`;
+        }
+        if(!countries.length) {
+            htmlStr = '<tr><td colspan="8" class="text-center">Не найдено</td></tr>'
+        }
+        $('table > tbody').html(htmlStr);
+        SelectTable();
+    };
+
+    const getData = () => {
+        let countries = [];
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then(res => res.json()).then(data => {
+                countries = data.map(country => {
+                    let {name, population, capital, area, currencies, languages, flag, region} = country;
+                    return {
+                        name, population, capital: capital || '---', flag, region: region || '---',
+                        area: area || 0,
+                        currencies: currencies.map(el => el.name).join(', '),
+                        languages: languages.map(el => el.name).join(', ')
+                    };
+                });
+                window.countries = countries;
+                renderCountries(countries);
+                renderOptions(countries);
+                setListeners();
+        });
+    }
+
+    getData();
+cd }
