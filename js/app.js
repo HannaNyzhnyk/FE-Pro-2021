9f25bfd50f8858aa79c27b11c1378cf6fbfe2c4b
nyzhnyk.ann@outlook.com <nyzhnyk.ann@outlook.com>
2021-06-02 21:40:56 +0300

    lection_91

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
-function Auto(brand, model, issue, vinCode) {
-    this.brand = brand;
-    this.model = model;
-    this.issue = issue;
-    this.vinCode = vinCode;
-}
-
-Auto.prototype.log = function() {
-    console.log(`марка: ${this.brand}, модель: ${this.model}, рік випуску: ${this.issue}, він-код: ${this.vinCode}`);
-}
-
-Auto.prototype.checkVin  = function() {
-    let code  = this.vinCode;
-    if (code.length === 16){
-        return true;
-    } else {
-        return false;
+window.onload = function() {
+    window.courses = null;
+    let now = new Date();
+    const selectDate  = () => {
+        $('#date_input').val(now.toISOString().slice(0, 10));
+        $('#date_input').change(function(e) {
+            var dateString = $(e.currentTarget).val();
+            var date = new Date(dateString)
+            if (date < now) {
+                getData(date);  
+            } else {
+                alert("Это будущий период");
+            }
+                    
+        });
     }
+
+    const renderCourses = сourses => {
+        let htmlStr = '';
+        for(let сourse of сourses) {
+            htmlStr += `<tr>
+            <td>${сourse.currency}</td>
+            <td>${сourse.rate}</td>
+            <td>${сourse.exchangedate}</td>
+        </tr>`;
+        }
+        
+        $('table > tbody').html(htmlStr);
+    };
+
+    const getData = (date) => {
+        let courses = [];
+        function format(date) {
+            var d = date.getDate();
+            var m = date.getMonth() + 1;
+            var y = date.getFullYear();
+            return '' + y + (m<=9 ? '0' + m : m) + (d <= 9 ? '0' + d : d);
+        }        
+        let formatedDate = format(date);
+        fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&date=${formatedDate}`)
+            .then((res) => res.json())
+            .then(data => {
+                console.log(data);
+                courses = data.map(сourse => {
+                    let {txt, rate, exchangedate} = сourse;
+                    return {
+                        exchangedate: exchangedate,
+                        rate: rate || 0,
+                        currency: txt
+                    };
+                });
+                window.courses = courses;
+                renderCourses(courses);
+            });
+    }
+    selectDate();
+    getData(now);
+
 }
 
-function Auto_Fuel(brand, model, issue, vinCode, engineVolume, consumption) {
-    this.brand = brand;
-    this.model = model;
-    this.issue = issue;
-    this.vinCode = vinCode;
-    this.engineVolume = engineVolume;
-    this.consumption = consumption;
-}
-
-Auto_Fuel.prototype = Object.create(Auto.prototype)
-
-Auto_Fuel.prototype.showFuelConsumption = function(){
-    return console.log(`объем двигателя:${this.engine_volume}, расход:${this.engineVolume}л.`)
-}
-
-function Auto_Electric(brand, model, issue, vinCode, batteryСapacity) {
-    this.brand = brand;
-    this.model = model;
-    this.issue = issue;
-    this.vinCode = vinCode;
-    this.batteryСapacity = batteryСapacity;
-}
-
-Auto_Electric.prototype = Object.create(Auto.prototype)
-
-Auto_Electric.prototype.showBatteryConfig = function(){
-    return console.log(`емкость батареи:${this.batteryСapacity}`)
-}
